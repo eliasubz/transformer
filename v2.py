a047dd195e7dd05861eaedc531c3ba09928f6c91
elias <eliasls2002@yahoo.de>
2025-09-11 14:47:22 +0200

    adding single and mulitheaded self-attention modules

--- v2.py
+++ v2.py
@@ -8,6 +8,9 @@ block_size = 8
 max_iters = 3000
 eval_interval = 300
 learning_rate = 1e-2
+n_embd = 32
+head_size = 32
+n_heads = 16
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 eval_iters = 200
@@ -65,21 +68,69 @@ def estimate_loss():
     return out
 
 
+class Head(nn.Module):
+    """one head of self-attention"""
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        v = self.value(x)
+        wei = q @ k.transpose(-2, -1) * C**-0.5
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)
+        # perform the weighted aggregation of the values
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    """multiple heads of self-attention in parallel"""
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
+class FeedForward(nn.Module):
+    """a simple linear layer followed by a non-linearity"""
+
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
     def __init__(self, vocab_size):
         super().__init__()
         # each token directly reads off the logits for the next token from a loopup table
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
-        self.position_embedding_table = nn.Embedding(block_size, vocab_size)
-
-    self.sa_heads = nn.ModuleList([Head(head_size) for _ in range(n_heads)])
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_heads = MultiHeadAttention(
+            4, n_embd // 4
+        )  # i.e. 4 heads of 8-dimensionsal self-attention
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
+        B, T = idx.shape
+
         # idx and targets are both (B, T) tensor of integers
-        logits = self.token_embedding_table(idx)  # (B,T,C)
-        if targets == None:
+        tok_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+        x = tok_emb + pos_emb
+        x = self.sa_heads(x)  # apply one head of self-attention. (B,T,C)
+        logits = self.lm_head(x)  # (B,T,C)
+
+        if targets is None:
             loss = None
         else:
             B, T, C = logits.shape
@@ -92,8 +143,10 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B,T) array of indices in the currect context
         for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
             # get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :]  # becomes(B, C)
             # apply softmax to get probabilities
@@ -128,31 +181,6 @@ for iter in range(max_iters):
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
-
-
-class Head(nn.Module):
-    """one head of self-attention"""
-
-    def __init__(self, head_size):
-        super().__init__()
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
-
-    def forward(self, x):
-        B, T, C = x.shape
-        k = self.key(x)
-        q = self.query(x)
-        v = self.value(x)
-        wei = q @ k.transpose(-2, -1) * C**-0.5
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
-        wei = F.softmax(wei, dim=-1)
-        # perform the weighted aggregation of the values
-        v = self.value(x)
-        out = wei @ v
-        return out
-
 
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
